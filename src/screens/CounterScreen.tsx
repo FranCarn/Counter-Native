119cbe0ad12f0551ab432b66e4e6ed47e477eb63
@FranCarn <carnevalefranco99@gmail.com>
2023-09-14 19:40:00 -0300

    using stylesheets

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,26 +1,19 @@
 import React, { useState } from 'react';
-import { TouchableOpacity, Text, View } from 'react-native';
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
 
 export const CounterScreen = () => {
   const [counter, setCounter] = useState(0);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        top: -15,
-      }}
-    >
-      <Text>Counter: {counter}</Text>
-      <View style={{ display: 'flex', flexDirection: 'row', gap: 10, marginTop: 10 }}>
+    <View style={styles.container}>
+      <Text style={{ fontSize: 30 }}>Counter: {counter}</Text>
+      <View style={styles.buttonsContainer}>
         <TouchableOpacity onPress={() => setCounter((prev) => prev + 1)}>
-          <View style={{ backgroundColor: 'black', borderRadius: 100 }}>
+          <View style={styles.button}>
             <Text>Increment</Text>
           </View>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => setCounter((prev) => prev - 1)}>
-          <View style={{ backgroundColor: 'black', borderRadius: 100 }}>
+          <View style={styles.button}>
             <Text>Decrement</Text>
           </View>
         </TouchableOpacity>
@@ -28,3 +21,14 @@ export const CounterScreen = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: -15,
+  },
+  buttonsContainer: { display: 'flex', flexDirection: 'row', gap: 10, marginTop: 10 },
+  button: { backgroundColor: '#eaeaea', borderRadius: 100, padding: 10 },
+});
