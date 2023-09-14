bc3318a7c3190c289275b4ea9be05a4e93728d02
@FranCarn <carnevalefranco99@gmail.com>
2023-09-14 19:50:55 -0300

    FAB ready

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -6,29 +6,49 @@ export const CounterScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 30 }}>Counter: {counter}</Text>
-      <View style={styles.buttonsContainer}>
-        <TouchableOpacity onPress={() => setCounter((prev) => prev + 1)}>
-          <View style={styles.button}>
-            <Text>Increment</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => setCounter((prev) => prev - 1)}>
-          <View style={styles.button}>
-            <Text>Decrement</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
+
+      <TouchableOpacity onPress={() => setCounter((prev) => prev + 1)} style={styles.fabLocationBR}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setCounter((prev) => prev - 1)} style={styles.fabLocationBL}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
     top: -15,
   },
-  buttonsContainer: { display: 'flex', flexDirection: 'row', gap: 10, marginTop: 10 },
-  button: { backgroundColor: '#eaeaea', borderRadius: 100, padding: 10 },
+  fabLocationBR: {
+    position: 'absolute',
+    right: 10,
+    bottom: 10,
+  },
+  fabLocationBL: {
+    position: 'absolute',
+    left: 10,
+    bottom: 10,
+  },
+  fab: {
+    backgroundColor: '#5856d6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  fabText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
 });
