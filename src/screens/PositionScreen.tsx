836c88ba59ea86af104165b192344892d5312c8f
@FranCarn <carnevalefranco99@gmail.com>
2023-09-17 21:17:30 -0300

    position ready

--- src/screens/PositionScreen.tsx
+++ src/screens/PositionScreen.tsx
@@ -44,8 +44,11 @@ const styles = StyleSheet.create({
     backgroundColor: 'green',
     borderWidth: 10,
     borderColor: 'white',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
+    // position: 'absolute',
+    // bottom: 0,
+    // left: 0,
+    // right: 0,
+    // top: 0
+    ...StyleSheet.absoluteFillObject,
   },
 });
