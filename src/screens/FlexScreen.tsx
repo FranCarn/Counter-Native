19bc6bd079c741f221aec8b93f8e82077820602e
@FranCarn <carnevalefranco99@gmail.com>
2023-09-18 16:37:18 -0300

    alignself?

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -4,31 +4,39 @@ import { StyleSheet, Text, View } from 'react-native';
 export const FlexScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.box1}>Caja 1</Text>
-      <Text style={styles.box2}>Caja 2</Text>
-      <Text style={styles.box3}>Caja 3</Text>
+      <Text style={styles.box1}>Box 1</Text>
+      <Text style={styles.box2}>Box 2</Text>
+      <Text style={styles.box3}>Box 3</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#28c4d9' },
+  container: {
+    flex: 1,
+    backgroundColor: '#28c4d9',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
   box1: {
-    flex: 2,
-    borderWidth: 2,
+    alignSelf: 'flex-start',
     borderColor: '#fff',
+    borderWidth: 2,
     fontSize: 20,
+    padding: 10,
   },
   box2: {
-    flex: 4,
-    borderWidth: 2,
+    alignSelf: 'center',
     borderColor: '#fff',
+    borderWidth: 2,
     fontSize: 20,
+    padding: 10,
   },
   box3: {
-    flex: 2,
-    borderWidth: 2,
+    alignSelf: 'flex-end',
     borderColor: '#fff',
+    borderWidth: 2,
     fontSize: 20,
+    padding: 10,
   },
 });
